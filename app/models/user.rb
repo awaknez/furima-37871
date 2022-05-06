3816e459d76216a428d405574c4b26f3a7102ca1
awaknez <zenz022021@gmail.com>
2022-05-07 08:44:38 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,29 @@ class User < ApplicationRecord
   has_many :purchases
   has_many :items
   
+  validates :nickname,                presence: true
+  validates :name_first_name,         presence: true,format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\d/} #ひらがなカタカナ漢字
+  validates :name_family_name,        presence: true,format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\d/} #ひらがなカタカナ漢字
+  validates :name_first_name_kana,    presence: true,format: { with:/\A[ァ-ヶ一]+\d/} #カタカナのみ
+  validates :name_family_name_kana,   presence: true,format: { with:/\A[ァ-ヶ一]+\d/} #カタカナのみ
+  validates :birthdat,                presence: true
+  validates :password,                               format: { with:/\A[a-z\d]{6,}\z/i} #英数字６字以上。大文字含む。
+  
+  
 end
+
+# c ... check , d ...default
+#ニックネームが必須であること。c
+#メールアドレスが必須であること。 d
+#メールアドレスが一意性であること。 d
+#メールアドレスは、@を含む必要があること。 d
+#パスワードが必須であること。 d
+#パスワードは、6文字以上での入力が必須であること d
+#パスワードは、半角英数字混合での入力が必須であること
+#パスワードとパスワード（確認）は、値の一致が必須であること。
+
+#お名前(全角)は、名字と名前がそれぞれ必須であること。 c
+#お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること。c
+#お名前カナ(全角)は、名字と名前がそれぞれ必須であること。 c
+#お名前カナ(全角)は、全角（カタカナ）での入力が必須であること。c
+#生年月日が必須であること。 c

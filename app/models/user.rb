aa54e6a0bfb08ac935b3165284a8b72ab2fa8e90
awaknez <zenz022021@gmail.com>
2022-05-07 14:35:03 +0900

    rspecの導入。userモデルの単体テストコード記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   has_many :items
   
   validates :nickname,                presence: true
-  validates :name_first_name,         presence: true,format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\d/} #ひらがなカタカナ漢字
-  validates :name_family_name,        presence: true,format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\d/} #ひらがなカタカナ漢字
-  validates :name_first_name_kana,    presence: true,format: { with:/\A[ァ-ヶ一]+\d/} #カタカナのみ
-  validates :name_family_name_kana,   presence: true,format: { with:/\A[ァ-ヶ一]+\d/} #カタカナのみ
-  validates :birthdat,                presence: true
+  validates :name_first_name,         presence: true,format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+/} #ひらがなカタカナ漢字
+  validates :name_family_name,        presence: true,format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+/} #ひらがなカタカナ漢字
+  validates :name_first_name_kana,    presence: true,format: { with:/\A[ァ-ヶ一]+\z/} #カタカナのみ
+  validates :name_family_name_kana,   presence: true,format: { with:/\A[ァ-ヶ一]+\z/} #カタカナのみ
+  validates :birthday,                presence: true
   validates :password,                               format: { with:/\A[a-z\d]{6,}\z/i} #英数字６字以上。大文字含む。
   
   

958e1a0b34f0931680332531b13bab62bba75e51
awaknez <zenz022021@gmail.com>
2022-05-12 11:13:54 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-        redirect_to item_path(item)
+        redirect_to item_path(@item)
     else
         render :edit    
     end
